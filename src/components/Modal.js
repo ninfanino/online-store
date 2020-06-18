2fd02726f6448176314559aa45e9e0759f190207
Ninfa Niño <ninfa_sys@hotmail.com>
2020-06-18 11:02:18 -0500

    css change

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -59,7 +59,7 @@ const ModalContainer = styled.div`
     left:0;
     right:0;
     bottom:0;
-    background: rgba(0,0,0,.3);
+    background: rgba(0,0,0,.4);
     display:flex;
     align-items:center;
     justify-content: center;
